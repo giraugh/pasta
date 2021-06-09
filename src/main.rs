fea0d79b6928ac304fefe119d8ef1ad5cbaeccdb
[Ewan Breakey s3845382] <s3845382@student.rmit.edu.au>
2021-06-09 20:24:20 +1000

    Cleanup debug printouts

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@ fn main() {
     let holidays = parse_holidays();
     let current_date = Local::now().format("%b%e").to_string();
     for holiday in holidays.iter() {
-        //println!("'{}' == '{}'", holiday.date.trim(), current_date.trim());
         if holiday.date.trim().eq(current_date.trim()) {
             println!("{}", holiday.holiday);
             break;
