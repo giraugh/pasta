2c89cb6a8ee2fe720cfd11f61b8425842c70b98b
[Ewan Breakey s3845382] <s3845382@student.rmit.edu.au>
2021-06-10 15:38:01 +1000

    Fix incorrect date fmt on 2-digit days

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use chrono::prelude::Local;
 
 fn main() {
     let holidays = parse_holidays();
-    let current_date = Local::now().format("%b%e").to_string();
+    let current_date = Local::now().format("%b %-e").to_string();
     for holiday in holidays.iter() {
         if holiday.date.trim().eq(current_date.trim()) {
             println!("{}", holiday.holiday);
